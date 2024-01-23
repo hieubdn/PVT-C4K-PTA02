b62cae5378a3a3181a5b1ec9abfbe5a68f460a7e
hieubdn <hieubdn@gmail.com>
2024-01-23 19:44:26 +0700

    update

--- Lesson 04/Checkpoint_1.py
+++ Lesson 04/Checkpoint_1.py
@@ -1,18 +1,18 @@
 # *****Tự luận*****
 # câu1
-# class Hinhvuong:
-#     def __init__(self, canh):
-#         self.canh = canh
+class Hinhvuong:
+    def __init__(self, canh):
+        self.canh = canh
 
-#     def tinh_chu_vi(self):
-#         chu_vi = 4 * self.canh
-#         return chu_vi
+    def tinh_chu_vi(self):
+        chu_vi = 4 * self.canh
+        return chu_vi
 
 
-# canh_hinh_vuong = 5
-# hinh_vuong = Hinhvuong(canh_hinh_vuong)
-# chu_vi_hinh_vuong = hinh_vuong.tinh_chu_vi()
-# print(f"Chu vi hình vuông có cạnh bằng {canh_hinh_vuong} là: {chu_vi_hinh_vuong}")
+canh_hinh_vuong = 5
+hinh_vuong = Hinhvuong(canh_hinh_vuong)
+chu_vi_hinh_vuong = hinh_vuong.tinh_chu_vi()
+print(f"Chu vi hình vuông có cạnh bằng {canh_hinh_vuong} là: {chu_vi_hinh_vuong}")
 
 # Câu 2
 class HocSinh:
